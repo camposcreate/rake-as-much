7e287216ba92e2da5fb3451d35d172426398a10e
camposcreate <uricampos123@gmail.com>
2024-07-23 19:07:35 -0500

    large/medium/small keywords now supported

--- src/main/resources/static/rake.js
+++ src/main/resources/static/rake.js
@@ -2,8 +2,8 @@ var largeStrings; // +3 words
 var mediumStrings; // 2 words
 var smallStrings; // 1 words
 
+// iterate keyword data and categorize (large, medium, small)
 function categorizeString(raw) {
-    // iterate keyword data and categorize
     raw.forEach((curr) => {
         if (curr.trim().split(/\s+/).length >= 3) {
             largeStrings.push(curr);
@@ -13,51 +13,93 @@ function categorizeString(raw) {
             smallStrings.push(curr);
         }
     });
-
 }
 
 function updateDisplay(data) {
-    // clear arrays if re-submit
+
+    // initialize arrays as empty
     largeStrings = [];
     mediumStrings = [];
     smallStrings = [];
 
-    categorizeString(data); // are keywords 1, 2, or 3+ words
+    categorizeString(data); // are keywords 1, 2, or 3+ words?
     //addClasses(); // add class to each keyword
 
     const display = document.getElementById('display');
     display.innerHTML = '';
 
-    // display data
-    if (data != null) {
-        display.classList.add('keywords');
-            const largeContainer = document.createElement('ul');
-            largeContainer.classList.add('largeContainer');
+    // large keywords list
+    if (largeStrings.length > 0) {
+        var h1Large = document.createElement('h1');
+        h1Large.textContent = 'Large Keywords';
 
-            largeStrings.forEach((large) => {
-                const li = document.createElement("li");
-                li.textContent = large;
-                li.classList.add('hoverable');
-                largeContainer.appendChild(li);
-            });
+        const largeContainer = document.createElement('ul');
+        largeContainer.classList.add('largeContainer');
 
-            // medium keywords list
-            const mediumContainer = document.createElement('ul');
-            mediumContainer.classList.add('mediumContainer');
+        largeStrings.forEach((large) => {
+            const li = document.createElement("li");
+            li.textContent = large;
+            li.classList.add('hoverable');
+            largeContainer.appendChild(li);
+        });
+        display.appendChild(h1Large);
+        display.appendChild(largeContainer);
+    }
 
-            mediumStrings.forEach((medium) => {
-                const li = document.createElement("li");
-                li.textContent = medium;
-                li.classList.add('hoverable');
-                largeContainer.appendChild(li);
-            });
+    // medium keywords list
+    if (mediumStrings.length > 0) {
+        var h1Medium = document.createElement('h1');
+        h1Medium.textContent = 'Medium Keywords';
 
-        display.appendChild(largeContainer, mediumContainer);
+        const mediumContainer = document.createElement('ul');
+        mediumContainer.classList.add('mediumContainer');
+
+        mediumStrings.forEach((medium) => {
+            const li = document.createElement("li");
+            li.textContent = medium;
+            li.classList.add('hoverable');
+            mediumContainer.appendChild(li);
+        });
+        display.appendChild(h1Medium);
+        display.appendChild(mediumContainer);
+    }
+
+    // small keywords list
+    if (smallStrings.length > 0) {
+        var h1Small = document.createElement('h1');
+        h1Small.textContent = 'Small Keywords';
+
+        const smallContainer = document.createElement('ul');
+        smallContainer.classList.add('smallContainer');
+
+        smallStrings.forEach((small) => {
+            const li = document.createElement("li");
+            li.textContent = small;
+            li.classList.add('hoverable');
+            smallContainer.appendChild(li);
+            console.log('printed small keywords:', small);
+        });
+        display.appendChild(h1Small);
+        display.appendChild(smallContainer);
+    }
+}
+
+function printUserInput(userInput) {
+    const userInputDisplay = document.getElementById('userDisplay');
+    userInputDisplay.innerHTML = '';
+
+    if (userInputDisplay != null) {
+        userInputDisplay.innerHTML = `
+            <p class='userInputData'>${userInput}</p>
+        `;
     }
 }
 
 function extractKeywords() {
+
     var input = document.getElementById('input').value.trim();
+    printUserInput(input);
+
     fetch('/api/extractKeywords', {
         method: 'POST',
         headers: {
